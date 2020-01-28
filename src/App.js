fac794f31818e7d9a2bae339dd2a7152b417341a
ricko_prihartanto <ricko.prihartanto@gmail.com>
2020-01-28 15:05:11 +0700

    wedding event

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, {PropTypes, Component } from 'react';
-import { Container, Carousel,Row, Col} from "react-bootstrap";
+import { Container, Carousel,Row, Col,Media} from "react-bootstrap";
 import { Parallax, Background } from 'react-parallax';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faHeart,faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+// import { faClock } from '@fortawesome/free-solid-svg-icons';
 
 // import { faHome } from "@fortawesome/free-solid-svg-icons";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -239,7 +240,82 @@ class App extends Component {
                         </div>
                     </Parallax>
                 </section>
+                
+                <section className="section-padding">
+                    <Container>
+                        <Row>
+                            <Col md="12">
+                                <div className="section-title flex justify-center mb-5">
+                                    <div className="icon-swan">
+                                        <img src={bird}/>
+                                        <div className="d-flex justify-content-center mt-min-16">
+                                            <div className="line-horizontal"></div>
+                                        </div>
+                                        <div className="d-flex justify-content-center">
+                                            <div className="line-vertikal"></div>
+                                        </div>
+                                        <div className="d-flex justify-content-center">
+                                            <div className="title-swan py-2 px-4">
+                                                <h2>Wedding Events</h2>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Col>
+                            <Col md="10" lg={{offset:"1"}} className="px-16">
+                                <Media className="mb-5">
+                                    <img className="mr-3" src="http://irsfoundation.com/tf/templates/wedding/lovely-wedding/lovely-wedding/images/events/img-1.jpg" alt="Generic placeholder"/>
+                                    <Media.Body>
+                                        <div className="details pl-4 pr-5">
+                                            <h3>Akad Nikah</h3>
+                                            <ul>
+                                                <li>
+                                                    <FontAwesomeIcon icon={faMapMarkerAlt} className="mr-2" size="md"/>
+                                                    {/* <i class="fa fa-map-marker mr-2" aria-hidden="true"></i> */}
+                                                    <span>32 big bro road, Chanpai, London.</span>
+                                                </li>
+                                                <li>
+                                                    <i className="fa fa-clock-o mr-2"></i>
+                                                    <span>Nov 25 2017, 9AM - 5PM</span>
+                                                </li>
+                                                
+                                            </ul>
+                                            <p>
+                                                when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                                            </p>
+                                            <a href="">See Location <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+                                        </div>
+                                    </Media.Body>
+                                </Media>
 
+                                <Media className="">
+                                    <img className="mr-3" src="http://irsfoundation.com/tf/templates/wedding/lovely-wedding/lovely-wedding/images/events/img-1.jpg" alt="Generic placeholder"/>
+                                    <Media.Body>
+                                        <div className="details pl-4 pr-5">
+                                            <h3>Resepsi Pernikahan</h3>
+                                            <ul>
+                                                <li>
+                                                    <FontAwesomeIcon icon={faMapMarkerAlt} className="mr-2" size="md"/>
+                                                    {/* <i class="fa fa-map-marker mr-2" aria-hidden="true"></i> */}
+                                                    <span>32 big bro road, Chanpai, London.</span>
+                                                </li>
+                                                <li>
+                                                    <i className="fa fa-clock-o mr-2"></i>
+                                                    <span>Nov 25 2017, 9AM - 5PM</span>
+                                                </li>
+                                                
+                                            </ul>
+                                            <p>
+                                                when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                                            </p>
+                                            <a href="">See Location <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+                                        </div>
+                                    </Media.Body>
+                                </Media>
+                            </Col>
+                        </Row>
+                    </Container>
+                </section>
              </div>
         </div>
 
